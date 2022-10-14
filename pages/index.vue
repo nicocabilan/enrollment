bec5b8452e00e63e2ef3683f5188d7b80c97965f
nicocabilan <nicocabilan0@gmail.com>
2022-10-14 21:03:18 +0800

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,56 @@
 <template>
 <center>
-<body style="background-color : powderblue;">
-<div class="row">
-  <div class="col s12 m8 l4 offset-m2 offset-l4">
-    <div class="card">
-      <div class="card-action teal lighten-1 white-text">
-        <h3>Login Form</h3>
+<section class="vh-100" style="background-color: #9A616D;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="OIP.jpg"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0s 0 1rem;" />
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+                <form>
+
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                    <span class="h1 fw-bold mb-0"><img src="logo.jpg" ></span>
+                  </div>
+
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Enrollment Login Form Here!!</h5>
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example17">Email</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example27">Password</label>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                  </div>
+
+                  <a class="small text-muted" href="#!">Forgot password?</a>
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
+                      style="color: #393f81;">Register here</a></p>
+                  <a href="#!" class="small text-muted">Terms of use.</a>
+                  <a href="#!" class="small text-muted">Privacy policy</a>
+                </form>
+
+              </div>
+            </div>
+          </div>
         </div>
-          <div class="card-content">
-        <div class="form-field">
-          <label for="username">Username</label>
-          <input type="text" id="username">
-        </div><br>
-
-        <div class="form-field">
-          <label for="password">Password</label>
-          <input type="password" id="password">
-        </div><br>
-
-        <div class="form-field">
-          <input type="checkbox" id="remem">
-          <label for="remem">Rememeber me</label>
-          
-        </div><br>
-
-        <div class="form-field">
-          <button class="btn-large waves-effect waves-dark" style="width:20%;">Login</button>
-        </div><br>
       </div>
+    </div>
   </div>
-</div>
-</div>
-</body>
+</section>
 </center>
 </template>
 <script>
